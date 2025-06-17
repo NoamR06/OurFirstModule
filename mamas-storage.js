ae2942ed59b01c84da767432765e44b8401bdbca
NoamR06 <noamreshef2006@gmail.com>
2025-06-17 16:45:43 +0300

    Fixed level 2

--- mamas-storage.js
+++ mamas-storage.js
@@ -11,9 +11,9 @@ export class InMemoryStorage{
         let newObject = item;
         const generated_uuid = uuidv4();
         newObject._uid = generated_uuid;
-        if (!(data_saver[typeof(item)][collectionName] === Array()))
-            data_saver[typeof(item)][collectionName] = [];
-        data_saver[typeof(item)][collectionName].push(newObject);
+        if (!(data_saver[item][collectionName] === Array()))
+            data_saver[item][collectionName] = [];
+        data_saver[item][collectionName].push(newObject);
         return newObject;
     }
     find(collectionName, findFunc){
@@ -49,7 +49,7 @@ export class InMemoryStorage{
          return true;
     }
     where(collectionName, where){
-        let collection = this.data_saver[typeof(where)][collectionName];
+        let collection = this.data_saver[where][collectionName];
         let data_to_return = [];
         collection.forEach(item => {
             if (this.#isEqual(item, where)){
@@ -80,25 +80,31 @@ export class InMemoryStorage{
 
 
 export class InMemorySharedStorage{
+    static shared_data = {};
+    
     constructor(){
-        let data_saver = {};
+        let data_saver = InMemorySharedStorage.shared_data;
     }
 
     create(collectionName, item){
         let newObject = item;
         const generated_uuid = uuidv4();
         newObject._uid = generated_uuid;
-        data_saver[collectionName].push(newObject);
+        if (!(data_saver[item][collectionName] === Array()))
+            data_saver[item][collectionName] = [];
+        data_saver[item][collectionName].push(newObject);
         return newObject;
     }
     find(collectionName, findFunc){
-        let collection = this.data_saver[collectionName];
+        const values = Object.values(this.data_saver);
         let data_to_return = [];
-        collection.forEach(item => {
-            if (findFunc(item)){
-                data_to_return.push(item);
-            }
+        for (let value in values){
+            if (value[collectionName] === Array()){
+                collection.forEach(item => {
+                    if (findFunc(item))
+                        data_to_return.push(item);
         });
+            }}
         return data_to_return;
     }
     #isEqual(obj1, obj2){
@@ -122,7 +128,7 @@ export class InMemorySharedStorage{
          return true;
     }
     where(collectionName, where){
-        let collection = this.data_saver[collectionName];
+        let collection = this.data_saver[where][collectionName];
         let data_to_return = [];
         collection.forEach(item => {
             if (this.#isEqual(item, where)){
@@ -132,18 +138,21 @@ export class InMemorySharedStorage{
         return data_to_return;
     }
     remove(collectionName, findFunc){
-        let collection = this.data_saver[collectionName];
+        const values = Object.values(this.data_saver);
         let data_to_remove = [];
-        let data_to_save = [];
-        collection.forEach(item => {
-            if (findFunc(item)){
-                data_to_remove.push(item);
-            }
-            else{
-                data_to_save.push(item)
-            }
-        });
-        this.data_saver[collectionName] = data_to_save;
+        for (let value in values){
+            let data_to_save = [];
+            if (value[collectionName] === Array()){
+                collection.forEach(item => {
+                    if (findFunc(item)){
+                        data_to_remove.push(item);
+                    }
+                    else{
+                        data_to_save.push(item)
+                    }
+                });
+                value[collectionName] = data_to_save;
+            }}
         return data_to_remove;
     }
 }
